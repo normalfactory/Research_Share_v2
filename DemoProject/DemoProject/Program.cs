35a683fbe70c1ab211771fb601d71f48240ad8aa
NormalFactory_Win <normalfactory@gmail.com>
2019-02-14 18:21:48 -0800

    Created the project for the first time.

---
+++ DemoProject/DemoProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NormalFactory.DemoProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is Scott");
+        }
+    }
+}
